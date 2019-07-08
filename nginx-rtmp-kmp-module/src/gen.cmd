3a0b78f2de45481bbade9f14b421af3610d6a7f0
erankor <eran.kornblau@kaltura.com>
2019-07-08 23:14:04 +0300

    nginx-rtmp-kmp: remove gen.cmd

--- nginx-rtmp-kmp-module/src/gen.cmd
+++
@@ -1,4 +0,0 @@
-python ../../nginx-common/scripts/generate_json_builder.py ngx_rtmp_kmp_json.txt > ngx_rtmp_kmp_json.h
-python ../../nginx-common/scripts/generate_json_builder.py ngx_rtmp_kmp_track_json.txt > ngx_rtmp_kmp_track_json.h
-python ../../nginx-common/scripts/generate_json_builder.py ngx_kmp_push_upstream_json.txt > ngx_kmp_push_upstream_json.h
-python ../../nginx-common/scripts/generate_json_builder.py ngx_kmp_push_track_json.txt > ngx_kmp_push_track_json.h
