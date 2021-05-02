kaltura/media-framework

commit 802d2da248d223db22076825b8cf5234477f3aca

ngx-live: media-persist stats fix
in case a read was successful but something else failed, the read would
have been counted as 'failed'.
also fixed a few warnings when using a newer gcc version.

nginx-live-module/src/persist/ngx_live_persist_media.c
@@ -523,17 +523,19 @@ ngx_live_persist_media_read_init_track(ngx_live_persist_media_read_ctx_t *ctx,
 }
 
 static void
-ngx_live_persist_media_read_complete(void *arg, ngx_int_t rc,
+ngx_live_persist_media_read_complete(void *arg, ngx_int_t code,
     ngx_buf_t *response)
 {
+    ngx_int_t                                 rc;
     ngx_str_t                                 buf;
     ngx_live_persist_media_read_ctx_t        *ctx = arg;
     ngx_live_persist_media_channel_ctx_t     *cctx;
     ngx_live_persist_media_read_track_ctx_t  *tctx;
 
-    if (rc != NGX_OK) {
+    if (code != NGX_OK) {
         ngx_log_error(NGX_LOG_NOTICE, ctx->pool->log, 0,
-            "ngx_live_persist_media_read_complete: read failed %i", rc);
+            "ngx_live_persist_media_read_complete: read failed %i", code);
+        rc = code;
         goto done;
     }
 
@@ -593,7 +595,7 @@ done:
         cctx = ngx_live_get_module_ctx(ctx->channel,
             ngx_live_persist_media_module);
 
-        if (rc == NGX_OK) {
+        if (code == NGX_OK) {
             cctx->read_stats.success++;
             cctx->read_stats.success_msec += ngx_current_msec - ctx->start;
             cctx->read_stats.success_size += ctx->size;
@@ -917,19 +919,21 @@ ngx_live_persist_media_copy_parse_header(
 }
 
 static void
-ngx_live_persist_media_copy_complete(void *arg, ngx_int_t rc,
+ngx_live_persist_media_copy_complete(void *arg, ngx_int_t code,
     ngx_buf_t *response)
 {
     ngx_buf_t                                *b;
+    ngx_int_t                                 rc;
     ngx_str_t                                 buf;
     ngx_chain_t                              *cl;
     ngx_live_persist_media_copy_ctx_t        *ctx = arg;
     ngx_live_persist_media_channel_ctx_t     *cctx;
     ngx_live_persist_media_read_track_ctx_t  *tctx;
 
-    if (rc != NGX_OK) {
+    if (code != NGX_OK) {
         ngx_log_error(NGX_LOG_NOTICE, ctx->pool->log, 0,
-            "ngx_live_persist_media_copy_complete: read failed %i", rc);
+            "ngx_live_persist_media_copy_complete: read failed %i", code);
+        rc = code;
         goto done;
     }
 
@@ -1009,7 +1013,7 @@ done:
         cctx = ngx_live_get_module_ctx(ctx->channel,
             ngx_live_persist_media_module);
 
-        if (rc == NGX_OK) {
+        if (code == NGX_OK) {
             cctx->read_stats.success++;
             cctx->read_stats.success_msec += ngx_current_msec - ctx->start;
             cctx->read_stats.success_size += ctx->size;
@@ -1350,26 +1354,26 @@ ngx_live_persist_media_write_complete(ngx_live_persist_write_file_ctx_t *ctx,
     ngx_int_t rc)
 {
     ngx_live_channel_t              *channel;
-    ngx_live_persist_media_scope_t   scope;
+    ngx_live_persist_media_scope_t  *scope;
 
     channel = ctx->channel;
-    scope = *(ngx_live_persist_media_scope_t *) ctx->scope;
+    scope = (void *) ctx->scope;
 
     if (rc != NGX_OK) {
         ngx_log_error(NGX_LOG_NOTICE, &channel->log, 0,
             "ngx_live_persist_media_write_complete: "
-            "write failed %i, bucket_id: %uD", rc, scope.bucket_id);
+            "write failed %i, bucket_id: %uD", rc, scope->bucket_id);
 
     } else {
         ngx_log_error(NGX_LOG_INFO, &channel->log, 0,
             "ngx_live_persist_media_write_complete: "
-            "write success, bucket_id: %uD", scope.bucket_id);
+            "write success, bucket_id: %uD", scope->bucket_id);
     }
 
     ngx_live_persist_write_file_destroy(ctx);
 
-    ngx_live_segment_index_persisted(channel, scope.min_index,
-        scope.max_index, rc);
+    ngx_live_segment_index_persisted(channel, scope->min_index,
+        scope->max_index, rc);
 }
 
 static void

nginx-live-module/src/persist/ngx_live_persist_setup.c
@@ -454,12 +454,12 @@ void
 ngx_live_persist_setup_write_complete(ngx_live_persist_write_file_ctx_t *ctx,
     ngx_int_t rc)
 {
-    uint32_t                               version;
+    uint32_t                              *version;
     ngx_live_channel_t                    *channel;
     ngx_live_persist_setup_channel_ctx_t  *cctx;
 
     channel = ctx->channel;
-    version = *(uint32_t *) ctx->scope;
+    version = (void *) ctx->scope;
 
     cctx = ngx_live_get_module_ctx(channel, ngx_live_persist_setup_module);
 
@@ -468,17 +468,17 @@ ngx_live_persist_setup_write_complete(ngx_live_persist_write_file_ctx_t *ctx,
     if (rc != NGX_OK) {
         ngx_log_error(NGX_LOG_NOTICE, &channel->log, 0,
             "ngx_live_persist_setup_write_complete: "
-            "write failed %i, version: %uD", rc, version);
+            "write failed %i, version: %uD", rc, *version);
 
     } else {
         ngx_log_error(NGX_LOG_INFO, &channel->log, 0,
             "ngx_live_persist_setup_write_complete: "
-            "write success, version: %uD", version);
+            "write success, version: %uD", *version);
 
-        cctx->success_version = version;
+        cctx->success_version = *version;
     }
 
-    if (version != cctx->version) {
+    if (*version != cctx->version) {
         ngx_add_timer(&cctx->timer, 1);
     }
 

nginx-pckg-module/src/ngx_pckg_ksmp.c
@@ -1259,6 +1259,9 @@ ngx_pckg_ksmp_create_request(ngx_pool_t *pool, ngx_pckg_ksmp_req_t *req,
 
         size += sizeof("&variant_ids=") - 1 + req->variant_ids.len +
             variants_escape;
+
+    } else {
+        variants_escape = 0;    /* suppress warning */
     }
 
     if (req->segment_index != NGX_KSMP_INVALID_SEGMENT_INDEX) {

commit a88acdb0aa73fae74fea9602501bafb85ae00fe5

ngx-live-persist-media: must copy scope to local (#557)
the scope is destroyed in ngx_live_persist_write_file_destroy
(bug introduced in 802d2da248d223db22076825b8cf5234477f3aca)

nginx-live-module/src/persist/ngx_live_persist_media.c
@@ -1354,26 +1354,27 @@ ngx_live_persist_media_write_complete(ngx_live_persist_write_file_ctx_t *ctx,
     ngx_int_t rc)
 {
     ngx_live_channel_t              *channel;
-    ngx_live_persist_media_scope_t  *scope;
+    ngx_live_persist_media_scope_t  *sp, scope;
 
     channel = ctx->channel;
-    scope = (void *) ctx->scope;
+    sp = (void *) ctx->scope;
+    scope = *sp;
 
     if (rc != NGX_OK) {
         ngx_log_error(NGX_LOG_NOTICE, &channel->log, 0,
             "ngx_live_persist_media_write_complete: "
-            "write failed %i, bucket_id: %uD", rc, scope->bucket_id);
+            "write failed %i, bucket_id: %uD", rc, scope.bucket_id);
 
     } else {
         ngx_log_error(NGX_LOG_INFO, &channel->log, 0,
             "ngx_live_persist_media_write_complete: "
-            "write success, bucket_id: %uD", scope->bucket_id);
+            "write success, bucket_id: %uD", scope.bucket_id);
     }
 
     ngx_live_persist_write_file_destroy(ctx);
 
-    ngx_live_segment_index_persisted(channel, scope->min_index,
-        scope->max_index, rc);
+    ngx_live_segment_index_persisted(channel, scope.min_index,
+        scope.max_index, rc);
 }
 
 static void