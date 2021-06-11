f5c1c0e96aa6247b414164e974ee04ce6dd9f3e6
erankor <eran.kornblau@kaltura.com>
2021-06-11 14:52:11 +0300

    ngx-live: add $live_version variable
    use it in persist_opaque for debugging purposes (identifies the version
    that created the file)

--- nginx-live-module/src/ngx_live_variables.c
+++ nginx-live-module/src/ngx_live_variables.c
@@ -9,12 +9,16 @@
 #include <ngx_core.h>
 #include "ngx_live.h"
 #include <nginx.h>
+#include <ngx_live_version.h>
+
 
 static ngx_live_variable_t *ngx_live_add_prefix_variable(ngx_conf_t *cf,
     ngx_str_t *name, ngx_uint_t flags);
 
 
 static ngx_int_t ngx_live_variable_nginx_version(ngx_live_variables_ctx_t *ctx,
+    ngx_live_variable_value_t *v, uintptr_t data);
+static ngx_int_t ngx_live_variable_live_version(ngx_live_variables_ctx_t *ctx,
     ngx_live_variable_value_t *v, uintptr_t data);
 static ngx_int_t ngx_live_variable_hostname(ngx_live_variables_ctx_t *ctx,
     ngx_live_variable_value_t *v, uintptr_t data);
@@ -32,6 +36,9 @@ static ngx_int_t ngx_live_variable_channel_uint32(
 static ngx_live_variable_t  ngx_live_core_variables[] = {
 
     { ngx_string("nginx_version"), NULL, ngx_live_variable_nginx_version,
+      0, 0, 0 },
+
+    { ngx_string("live_version"), NULL, ngx_live_variable_live_version,
       0, 0, 0 },
 
     { ngx_string("hostname"), NULL, ngx_live_variable_hostname,
@@ -424,6 +431,20 @@ ngx_live_variable_nginx_version(ngx_live_variables_ctx_t *ctx,
     v->no_cacheable = 0;
     v->not_found = 0;
     v->data = (u_char *) NGINX_VERSION;
+
+    return NGX_OK;
+}
+
+
+static ngx_int_t
+ngx_live_variable_live_version(ngx_live_variables_ctx_t *ctx,
+    ngx_live_variable_value_t *v, uintptr_t data)
+{
+    v->len = sizeof(NGX_LIVE_VERSION) - 1;
+    v->valid = 1;
+    v->no_cacheable = 0;
+    v->not_found = 0;
+    v->data = (u_char *) NGX_LIVE_VERSION;
 
     return NGX_OK;
 }
