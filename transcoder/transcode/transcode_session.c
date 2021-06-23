1283eb58d281e758e6fcba91db318329620b5606
igormacpro <igor.shevach@kaltura.com>
2021-06-23 19:18:15 +0300

    LIV-707: aspect ratio not good - defer encoder+filter initiation until after first frame is received by decoder, since pps not always relevant at the time of decoder initialization (>1 sets of sps/pps)

--- transcoder/transcode/transcode_session.c
+++ transcoder/transcode/transcode_session.c
@@ -260,6 +260,34 @@ int config_encoder(transcode_session_output_t *pOutput,  transcode_codec_t *pDec
     return ret;
 }
 
+static
+int transcode_session_init_output(transcode_session_t* pContext,
+    transcode_codec_t *pDecoderContext,
+    transcode_session_output_t* pOutput)
+{
+    transcode_filter_t* pFilter=GetFilter(pContext,pOutput,pDecoderContext);
+    transcode_codec_t* pEncoderContext=&pContext->encoder[pContext->encoders];
+
+    _S(config_encoder(pOutput, pDecoderContext, pFilter, pEncoderContext));
+
+    pOutput->encoderId=pContext->encoders++;
+    LOGGER(CATEGORY_TRANSCODING_SESSION,AV_LOG_INFO,"Output %s - Added encoder %d bitrate=%d",pOutput->track_id,pOutput->encoderId,pOutput->bitrate);
+
+    transcode_mediaInfo_t extra;
+    extra.frameRate=pEncoderContext->ctx->framerate;
+    extra.timeScale=pEncoderContext->ctx->time_base;
+    extra.codecParams=avcodec_parameters_alloc();
+    //TODO: how do we know encoder supports captions?
+    extra.closed_captions = pContext->currentMediaInfo->closed_captions;
+    avcodec_parameters_from_context(extra.codecParams,pEncoderContext->ctx);
+    if(!extra.codecParams->bits_per_coded_sample) {
+        extra.codecParams->bits_per_coded_sample = pDecoderContext->ctx->bits_per_coded_sample;
+    }
+    _S(transcode_session_output_set_media_info(pOutput,&extra,pContext->input_frame_first_id));
+
+    return 0;
+}
+
 int transcode_session_add_output(transcode_session_t* pContext, const json_value_t* json )
 {
     transcode_codec_t *pDecoderContext=&pContext->decoder[0];
@@ -268,31 +296,7 @@ int transcode_session_add_output(transcode_session_t* pContext, const json_value
     strcpy(pOutput->channel_id,pContext->channelId);
     int ret=0;
     
-    if (!pOutput->passthrough)
-    {
-        transcode_filter_t* pFilter=GetFilter(pContext,pOutput,pDecoderContext);
-        transcode_codec_t* pEncoderContext=&pContext->encoder[pContext->encoders];
-        
-        ret=config_encoder(pOutput, pDecoderContext, pFilter, pEncoderContext);
-        if (ret<0) {
-            return ret;
-        }
-        
-        pOutput->encoderId=pContext->encoders++;
-        LOGGER(CATEGORY_TRANSCODING_SESSION,AV_LOG_INFO,"Output %s - Added encoder %d bitrate=%d",pOutput->track_id,pOutput->encoderId,pOutput->bitrate);
-        
-        transcode_mediaInfo_t extra;
-        extra.frameRate=pEncoderContext->ctx->framerate;
-        extra.timeScale=pEncoderContext->ctx->time_base;
-        extra.codecParams=avcodec_parameters_alloc();
-        //TODO: how do we know encoder supports captions?
-        extra.closed_captions = pContext->currentMediaInfo->closed_captions;
-        avcodec_parameters_from_context(extra.codecParams,pEncoderContext->ctx);
-        if(!extra.codecParams->bits_per_coded_sample) {
-            extra.codecParams->bits_per_coded_sample = pDecoderContext->ctx->bits_per_coded_sample;
-        }
-        _S(transcode_session_output_set_media_info(pOutput,&extra,pContext->input_frame_first_id));
-    } else
+    if (pOutput->passthrough)
     {
         _S(transcode_session_output_set_media_info(pOutput,pContext->currentMediaInfo,pContext->input_frame_first_id));
     }
@@ -485,6 +489,13 @@ int sendFrameToFilter(transcode_session_t *pContext,int filterId, AVCodecContext
 
 int OnDecodedFrame(transcode_session_t *ctx,AVCodecContext* decoderCtx, AVFrame *frame)
 {
+   for (int outputId=0;outputId<ctx->outputs;outputId++) {
+         transcode_session_output_t *pOutput=&ctx->output[outputId];
+         if (!pOutput->passthrough && pOutput->encoderId==-1) {
+              _S(transcode_session_init_output(ctx,&ctx->decoder[0],pOutput));
+         }
+    }
+
     if (frame==NULL) {
         
         for (int outputId=0;outputId<ctx->outputs;outputId++) {
