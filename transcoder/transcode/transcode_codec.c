2ef170940f8f4dfcfcdeb4054b766f84a2e122f9
igormacpro <igor.shevach@kaltura.com>
2021-06-24 12:57:06 +0300

    - fixed inter-branch code mingling

--- transcoder/transcode/transcode_codec.c
+++ transcoder/transcode/transcode_codec.c
@@ -177,7 +177,6 @@ int transcode_codec_init_decoder( transcode_codec_t * pContext,transcode_mediaIn
     pContext->ctx = codec_ctx;
     //codec_ctx->time_base=extraParams->timeScale;
     if (codec_ctx->codec_type==AVMEDIA_TYPE_VIDEO) {
-        _S(check_video_decoder_ar(codec_ctx));
         LOGGER(CATEGORY_CODEC,AV_LOG_INFO, "Initialized video decoder \"%s\" color space: %s width %d height %d ar %d/%d extra sz %d",
             dec->long_name, av_get_pix_fmt_name (codec_ctx->pix_fmt),
             codec_ctx->width,codec_ctx->height,
