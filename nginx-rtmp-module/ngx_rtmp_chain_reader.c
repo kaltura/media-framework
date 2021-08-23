860f0368ba8e275b664eb5008976e231f6f68755
erankor <eran.kornblau@kaltura.com>
2021-08-24 00:04:27 +0300

    ngx-rtmp: improve cea caption detection
    - scan all payloads within each sei nal unit
    - ignore emulation prevention bytes within the nal unit

--- nginx-rtmp-module/ngx_rtmp_chain_reader.c
+++ nginx-rtmp-module/ngx_rtmp_chain_reader.c
@@ -63,3 +63,102 @@ ngx_rtmp_chain_reader_skip(ngx_rtmp_chain_reader_t *reader, size_t size)
         reader->pos = reader->buf->pos;
     }
 }
+
+
+void
+ngx_rtmp_chain_reader_ep_init(ngx_rtmp_chain_reader_ep_t *reader,
+    ngx_rtmp_chain_reader_t *src)
+{
+    reader->base = *src;
+    reader->last_three = 1;
+}
+
+ngx_int_t
+ngx_rtmp_chain_reader_ep_read(ngx_rtmp_chain_reader_ep_t *reader,
+    u_char *dst, size_t size)
+{
+    u_char   b;
+    u_char  *dst_end;
+
+    if (size > reader->left) {
+        return NGX_ERROR;
+    }
+    reader->left -= size;
+
+    dst_end = dst + size;
+    while (dst < dst_end) {
+
+        for ( ;; ) {
+
+            if (reader->base.pos < reader->base.buf->last) {
+                b = *reader->base.pos++;
+                break;
+            }
+
+            if (reader->base.cl->next == NULL) {
+                return NGX_ERROR;
+            }
+
+            reader->base.cl = reader->base.cl->next;
+            reader->base.buf = reader->base.cl->buf;
+            reader->base.pos = reader->base.buf->pos;
+        }
+
+        reader->last_three = ((reader->last_three << 8) | b) & 0xffffff;
+        if (reader->last_three == 3) {
+            if (reader->left <= 0) {
+                return NGX_ERROR;
+            }
+            reader->left--;
+            continue;
+        }
+
+        *dst++ = b;
+    }
+
+    return NGX_OK;
+}
+
+ngx_int_t
+ngx_rtmp_chain_reader_ep_skip(ngx_rtmp_chain_reader_ep_t *reader, size_t size)
+{
+    u_char  b;
+
+    if (size > reader->left) {
+        return NGX_ERROR;
+    }
+    reader->left -= size;
+
+    while (size > 0) {
+
+        for ( ;; ) {
+
+            if (reader->base.pos < reader->base.buf->last) {
+                b = *reader->base.pos++;
+                break;
+            }
+
+            if (reader->base.cl->next == NULL) {
+                return NGX_ERROR;
+            }
+
+            reader->base.cl = reader->base.cl->next;
+            reader->base.buf = reader->base.cl->buf;
+            reader->base.pos = reader->base.buf->pos;
+        }
+
+
+        reader->last_three = ((reader->last_three << 8) | b) & 0xffffff;
+        if (reader->last_three == 3) {
+            if (reader->left <= 0) {
+                return NGX_ERROR;
+            }
+            reader->left--;
+            continue;
+        }
+
+        size--;
+    }
+
+    return NGX_OK;
+}
