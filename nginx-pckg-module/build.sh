74881e4899d16ec723f26e4e522194e3fad7a055
Amirhalfon1 <amir.halfon@kaltura.com>
2021-10-25 11:30:33 +0300

    rename image name from "front-live" to "live-front" (#988)
    rename image name from "front-live" to "live-front" to be aligned with all other component names
    this PR depends on devops - need to change this line https://github.com/kaltura/ovp-tools/blob/4f35027a56f581ab5d795067c0d917116ee90784/jenkins/jobs/live/build_jobs/Jenkinsfile_build_front#L52

--- nginx-pckg-module/build.sh
+++ nginx-pckg-module/build.sh
@@ -1,24 +1,24 @@
 OPENRESTY_VERSION="1.17.8.2"
-echo "building  kaltura/front-live-src"
-docker build -t kaltura/front-live-src:latest -f nginx-pckg-module/Dockerfile-front-live-src .
+echo "building  kaltura/live-front-src"
+docker build -t kaltura/live-front-src:latest -f nginx-pckg-module/Dockerfile-live-front-src .
 if [ $? -ne 0 ]
 then
-  echo "failed to build kaltura/front-live-src" >&2
+  echo "failed to build kaltura/live-front-src" >&2
   exit 1
 fi
-echo "Successfully built kaltura/front-live-src"
+echo "Successfully built kaltura/live-front-src"
 if ! [ -d 'docker-openresty' ]; then git clone https://github.com/openresty/docker-openresty.git; fi
 cd docker-openresty
-docker build -t kaltura/front-live:$tag -t kaltura/front-live:latest \
+docker build -t kaltura/live-front:$tag -t kaltura/live-front:latest \
 --build-arg RESTY_VERSION="$OPENRESTY_VERSION" \
---build-arg RESTY_IMAGE_BASE="kaltura/front-live-src" \
+--build-arg RESTY_IMAGE_BASE="kaltura/live-front-src" \
 --build-arg RESTY_IMAGE_TAG="latest" \
 --build-arg RESTY_EVAL_POST_MAKE="rm -rf /tmp/nginx-secure-token-module /tmp/nginx_mod_akamai_g2o /tmp/nginx-common /tmp/nginx-pckg-module" \
 --build-arg RESTY_CONFIG_OPTIONS_MORE="--add-dynamic-module=/tmp/nginx-secure-token-module --add-dynamic-module=/tmp/nginx_mod_akamai_g2o --add-dynamic-module=/tmp/nginx-common --add-dynamic-module=/tmp/nginx-pckg-module" \
 . -f alpine/Dockerfile
 if [ $? -ne 0 ]
 then
-  echo "failed to build kaltura/front-live" >&2
+  echo "failed to build kaltura/live-front" >&2
   exit 1
 fi
-echo "Successfully built kaltura/front-live"
+echo "Successfully built kaltura/live-front"
