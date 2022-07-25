91818e44410bb21aeb750f5ea3f5ceecd40cee53
erankor <eran.kornblau@kaltura.com>
2022-07-25 18:04:50 +0300

    ngx-live: add test for end_list=forced

---
+++ nginx-live-module/test/tests/ll_force_end_list.py
@@ -0,0 +1,31 @@
+from test_base import *
+
+def updateConf(conf):
+    block = getConfBlock(conf, ['live', 'preset ll'])
+    block.append(['ll_segmenter_inactive_timeout', '10'])
+
+FORCED_TIMELINE_ID = 'forced'
+
+def test(channelId=CHANNEL_ID):
+    st = KmpSendTimestamps()
+
+    nl = setupChannelTimeline(channelId, preset=LL_PRESET)
+    nl.timeline.create(NginxLiveTimeline(id=FORCED_TIMELINE_ID, active=True))
+
+    rv = KmpMediaFileReader(TEST_VIDEO1, 0)
+    ra = KmpMediaFileReader(TEST_VIDEO1, 1)
+
+    sv, sa = createVariant(nl, 'var1', [('v1', 'video'), ('a1', 'audio')])
+
+    kmpSendStreams([
+        (rv, sv),
+        (ra, sa),
+    ], st, 10.5)
+
+    nl.timeline.update(NginxLiveTimeline(id=TIMELINE_ID, end_list='on'))
+    nl.timeline.update(NginxLiveTimeline(id=FORCED_TIMELINE_ID, end_list='forced'))
+
+    url = getStreamUrl(channelId, 'hls-ll', timelineId=FORCED_TIMELINE_ID)
+    testStream(url, __file__, 'hls-ll-' + FORCED_TIMELINE_ID)
+
+    testLLDefaultStreams(channelId, __file__)
