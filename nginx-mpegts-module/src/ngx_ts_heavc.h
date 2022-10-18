42f5b9dab33de8eb4229364533080b99d4305c8e
erankor <eran.kornblau@kaltura.com>
2022-10-18 14:23:22 +0300

    ngx-ts-heavc: align func/type names with module

--- nginx-mpegts-module/src/ngx_ts_heavc.h
+++ nginx-mpegts-module/src/ngx_ts_heavc.h
@@ -6,8 +6,8 @@
 #include <ngx_core.h>
 
 
-#define ngx_ts_avc_skip_u   ngx_ts_avc_read_u
-#define ngx_ts_avc_skip_u1  ngx_ts_avc_read_u1
+#define ngx_ts_heavc_skip_u   ngx_ts_heavc_read_u
+#define ngx_ts_heavc_skip_u1  ngx_ts_heavc_read_u1
 
 
 typedef struct {
@@ -18,35 +18,35 @@ typedef struct {
     uint32_t     last_three;
     ngx_log_t   *log;
     char        *codec;
-} ngx_ts_avc_reader_t;
+} ngx_ts_heavc_reader_t;
 
 
-void ngx_ts_avc_init_reader(ngx_ts_avc_reader_t *br, u_char *buf, size_t len,
-    ngx_log_t *log, char *codec);
+void ngx_ts_heavc_init_reader(ngx_ts_heavc_reader_t *br, u_char *buf,
+    size_t len, ngx_log_t *log, char *codec);
 
 
-uint32_t ngx_ts_avc_read_one(ngx_ts_avc_reader_t *br);
+uint32_t ngx_ts_heavc_read_one(ngx_ts_heavc_reader_t *br);
 
-uint64_t ngx_ts_avc_read(ngx_ts_avc_reader_t *br, ngx_uint_t bits);
+uint64_t ngx_ts_heavc_read(ngx_ts_heavc_reader_t *br, ngx_uint_t bits);
 
-uint64_t ngx_ts_avc_read_golomb_unsigned(ngx_ts_avc_reader_t *br);
+uint64_t ngx_ts_heavc_read_golomb_unsigned(ngx_ts_heavc_reader_t *br);
 
-int64_t ngx_ts_avc_read_golomb_signed(ngx_ts_avc_reader_t *br);
-
-
-ngx_flag_t ngx_ts_avc_rbsp_trailing_bits(ngx_ts_avc_reader_t *br);
+int64_t ngx_ts_heavc_read_golomb_signed(ngx_ts_heavc_reader_t *br);
 
 
-ngx_flag_t ngx_ts_avc_sei_detect_cea(ngx_log_t *log, ngx_chain_t *in,
+ngx_flag_t ngx_ts_heavc_rbsp_trailing_bits(ngx_ts_heavc_reader_t *br);
+
+
+ngx_flag_t ngx_ts_heavc_sei_detect_cea(ngx_log_t *log, ngx_chain_t *in,
     u_char *pos, size_t size, size_t nal_header_size);
 
 
 static ngx_inline uint32_t
-ngx_ts_avc_read_u1(ngx_ts_avc_reader_t *br, char *name)
+ngx_ts_heavc_read_u1(ngx_ts_heavc_reader_t *br, char *name)
 {
     uint32_t  v;
 
-    v = ngx_ts_avc_read_one(br);
+    v = ngx_ts_heavc_read_one(br);
 
     ngx_log_debug3(NGX_LOG_DEBUG_CORE, br->log, 0,
         "ts %s u(1) %s:%uD", br->codec, name, v);
@@ -56,11 +56,11 @@ ngx_ts_avc_read_u1(ngx_ts_avc_reader_t *br, char *name)
 
 
 static ngx_inline uint64_t
-ngx_ts_avc_read_u(ngx_ts_avc_reader_t *br, ngx_uint_t bits, char *name)
+ngx_ts_heavc_read_u(ngx_ts_heavc_reader_t *br, ngx_uint_t bits, char *name)
 {
     uint64_t  v;
 
-    v = ngx_ts_avc_read(br, bits);
+    v = ngx_ts_heavc_read(br, bits);
 
     ngx_log_debug4(NGX_LOG_DEBUG_CORE, br->log, 0,
         "ts %s u(%ui) %s:%uL", br->codec, bits, name, v);
@@ -70,11 +70,11 @@ ngx_ts_avc_read_u(ngx_ts_avc_reader_t *br, ngx_uint_t bits, char *name)
 
 
 static ngx_inline uint64_t
-ngx_ts_avc_read_ue(ngx_ts_avc_reader_t *br, char *name)
+ngx_ts_heavc_read_ue(ngx_ts_heavc_reader_t *br, char *name)
 {
     uint64_t  v;
 
-    v = ngx_ts_avc_read_golomb_unsigned(br);
+    v = ngx_ts_heavc_read_golomb_unsigned(br);
 
     ngx_log_debug3(NGX_LOG_DEBUG_CORE, br->log, 0,
         "ts %s ue(v) %s:%uL", br->codec, name, v);
@@ -84,11 +84,11 @@ ngx_ts_avc_read_ue(ngx_ts_avc_reader_t *br, char *name)
 
 
 static ngx_inline int64_t
-ngx_ts_avc_read_se(ngx_ts_avc_reader_t *br, char *name)
+ngx_ts_heavc_read_se(ngx_ts_heavc_reader_t *br, char *name)
 {
     int64_t  v;
 
-    v = ngx_ts_avc_read_golomb_signed(br);
+    v = ngx_ts_heavc_read_golomb_signed(br);
 
     ngx_log_debug3(NGX_LOG_DEBUG_CORE, br->log, 0,
         "ts %s se(v) %s:%L", br->codec, name, v);
