e5b7eff1b867c685fb69648c7a7dca00a2bcbdd2
erankor <eran.kornblau@kaltura.com>
2022-11-06 21:21:29 +0200

    ngx-rtmp-kmp: align api impl
    break the api into two parts - rtmp & http

--- nginx-rtmp-kmp-module/src/ngx_rtmp_kmp_api_json.h
+++ nginx-rtmp-kmp-module/src/ngx_rtmp_kmp_api_json.h
@@ -257,10 +257,10 @@ ngx_rtmp_kmp_api_server_json_write(u_char *p, ngx_rtmp_core_srv_conf_t *obj)
 }
 
 
-/* ngx_rtmp_kmp_api_json writer */
+/* ngx_rtmp_kmp_api_servers_json writer */
 
-static size_t
-ngx_rtmp_kmp_api_json_get_size(void *obj)
+size_t
+ngx_rtmp_kmp_api_servers_json_get_size(void *obj)
 {
     size_t                      result;
     ngx_uint_t                  n;
@@ -268,26 +268,13 @@ ngx_rtmp_kmp_api_json_get_size(void *obj)
     ngx_rtmp_core_main_conf_t  *cmcf;
 
     cmcf = ngx_rtmp_core_main_conf;
+    if (!cmcf) {
+        return sizeof("null") - 1;
+    }
+
     result =
-        sizeof("{\"version\":\"") - 1 +
-            ngx_json_str_get_size(&ngx_rtmp_kmp_version) +
-        sizeof("\",\"nginx_version\":\"") - 1 +
-            ngx_json_str_get_size(&ngx_rtmp_kmp_nginx_version) +
-        sizeof("\",\"rtmp_version\":\"") - 1 +
-            ngx_json_str_get_size(&ngx_rtmp_kmp_rtmp_version) +
-        sizeof("\",\"compiler\":\"") - 1 +
-            ngx_json_str_get_size(&ngx_rtmp_kmp_compiler) +
-        sizeof("\",\"built\":\"") - 1 +
-            ngx_json_str_get_size(&ngx_rtmp_kmp_built) +
-        sizeof("\",\"pid\":") - 1 + NGX_INT_T_LEN +
-        sizeof(",\"uptime\":") - 1 + NGX_INT_T_LEN +
-        sizeof(",\"naccepted\":") - 1 + NGX_INT_T_LEN +
-        sizeof(",\"bw_in\":") - 1 + NGX_INT64_LEN +
-        sizeof(",\"bytes_in\":") - 1 + NGX_INT64_LEN +
-        sizeof(",\"bw_out\":") - 1 + NGX_INT64_LEN +
-        sizeof(",\"bytes_out\":") - 1 + NGX_INT64_LEN +
-        sizeof(",\"servers\":[") - 1 +
-        sizeof("]}") - 1;
+        sizeof("[") - 1 +
+        sizeof("]") - 1;
 
     for (n = 0; n < cmcf->servers.nelts; n++) {
         cur = ((ngx_rtmp_core_srv_conf_t **) cmcf->servers.elts)[n];
@@ -299,40 +286,20 @@ ngx_rtmp_kmp_api_json_get_size(void *obj)
 }
 
 
-static u_char *
-ngx_rtmp_kmp_api_json_write(u_char *p, void *obj)
+u_char *
+ngx_rtmp_kmp_api_servers_json_write(u_char *p, void *obj)
 {
     ngx_uint_t                  n;
     ngx_rtmp_core_srv_conf_t   *cur;
     ngx_rtmp_core_main_conf_t  *cmcf;
 
     cmcf = ngx_rtmp_core_main_conf;
-    p = ngx_copy_fix(p, "{\"version\":\"");
-    p = ngx_json_str_write(p, &ngx_rtmp_kmp_version);
-    p = ngx_copy_fix(p, "\",\"nginx_version\":\"");
-    p = ngx_json_str_write(p, &ngx_rtmp_kmp_nginx_version);
-    p = ngx_copy_fix(p, "\",\"rtmp_version\":\"");
-    p = ngx_json_str_write(p, &ngx_rtmp_kmp_rtmp_version);
-    p = ngx_copy_fix(p, "\",\"compiler\":\"");
-    p = ngx_json_str_write(p, &ngx_rtmp_kmp_compiler);
-    p = ngx_copy_fix(p, "\",\"built\":\"");
-    p = ngx_json_str_write(p, &ngx_rtmp_kmp_built);
-    p = ngx_copy_fix(p, "\",\"pid\":");
-    p = ngx_sprintf(p, "%ui", (ngx_uint_t) ngx_getpid());
-    p = ngx_copy_fix(p, ",\"uptime\":");
-    p = ngx_sprintf(p, "%i", (ngx_int_t) (ngx_cached_time->sec -
-        ngx_rtmp_kmp_start_time));
-    p = ngx_copy_fix(p, ",\"naccepted\":");
-    p = ngx_sprintf(p, "%ui", (ngx_uint_t) ngx_rtmp_naccepted);
-    p = ngx_copy_fix(p, ",\"bw_in\":");
-    p = ngx_sprintf(p, "%uL", (uint64_t) (ngx_rtmp_bw_in.bandwidth * 8));
-    p = ngx_copy_fix(p, ",\"bytes_in\":");
-    p = ngx_sprintf(p, "%uL", (uint64_t) ngx_rtmp_bw_in.bytes);
-    p = ngx_copy_fix(p, ",\"bw_out\":");
-    p = ngx_sprintf(p, "%uL", (uint64_t) (ngx_rtmp_bw_out.bandwidth * 8));
-    p = ngx_copy_fix(p, ",\"bytes_out\":");
-    p = ngx_sprintf(p, "%uL", (uint64_t) ngx_rtmp_bw_out.bytes);
-    p = ngx_copy_fix(p, ",\"servers\":[");
+    if (!cmcf) {
+        p = ngx_copy_fix(p, "null");
+        return p;
+    }
+
+    *p++ = '[';
 
     for (n = 0; n < cmcf->servers.nelts; n++) {
         cur = ((ngx_rtmp_core_srv_conf_t **) cmcf->servers.elts)[n];
@@ -344,7 +311,7 @@ ngx_rtmp_kmp_api_json_write(u_char *p, void *obj)
         p = ngx_rtmp_kmp_api_server_json_write(p, cur);
     }
 
-    p = ngx_copy_fix(p, "]}");
+    *p++ = ']';
 
     return p;
 }
