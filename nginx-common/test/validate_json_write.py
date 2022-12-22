0be90a58033d3e06c46f8ca2aa14a084b79d555a
erankor <eran.kornblau@kaltura.com>
2022-12-22 17:56:15 +0200

    ngx-common: add validation on json writes
    code generated by generate_json_header.py is guaranteed to be consistent
    between the 'write' func and the 'get_size' func.
    however, hand-written code may get out of sync. the script collects all
    strings written using ngx_copy_fix, and makes sure the code contains a
    matching sizeof.

---
+++ nginx-common/test/validate_json_write.py
@@ -0,0 +1,35 @@
+import sys
+import re
+import os
+
+def get_quoted_str(s):
+    start = s.find('"')
+    if start < 0:
+        return None
+
+    end = s.rfind('"')
+    s = s[(start + 1):end]
+
+    return re.sub(r'(?<!\\)"\s+"', '', s)
+
+def process_file(file_path):
+    file_data = open(file_path, 'r').read()
+
+    sizeofs = set(map(get_quoted_str, re.findall('sizeof\(([^)]*)\)', file_data)))
+    copies = set(map(get_quoted_str, re.findall('ngx_copy_fix\(([^)]*)\)', file_data)))
+    
+    missing_sizeof = copies - sizeofs - set(["true"])
+    for s in missing_sizeof:
+        print('Error: missing sizeof for %s in %s' % (s, file_path))
+
+
+root_folder = os.path.join(os.path.dirname(__file__), '../..')
+root_folder = os.path.normpath(root_folder)
+
+for root, _, files in os.walk(root_folder):
+    for name in files:
+        if os.path.splitext(name)[1] not in ['.c', '.h']:
+            continue
+
+        file_path = os.path.join(root, name)
+        process_file(file_path)
